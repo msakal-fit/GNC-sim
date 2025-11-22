cdae1558d8d01a6000da79f57d3df562d2eaa573
msakal2024 <msakal2024@my.fit.edu>
2025-11-22 15:27:27 -0500

    Update reinitial_x500.m

--- MATLAB/GNC_Algorithms/Control/reinitial_x500.m
+++ MATLAB/GNC_Algorithms/Control/reinitial_x500.m
@@ -1,28 +1,31 @@
-config = px4_get_config();
-client = px4_connect(config.ip_address, config.port);
+function reinitial_x500()
+    % Reinitializes the X500 drone to a known state.
+    config = px4_get_config();
+    client = px4_connect(config.ip_address, config.port);
 
-px4_enter_offboard_mode(client, config);
-pause(2);
+    px4_enter_offboard_mode(client, config);
+    pause(2);
 
-px4_arm_drone(client, config);
-pause(2);
+    px4_arm_drone(client, config);
+    pause(2);
 
-% MODIFY TAKEOFF ALTITUDE, IF NEEDED.
-config.takeoff_altitude = -2.0;
+    % MODIFY TAKEOFF ALTITUDE, IF NEEDED.
+    config.takeoff_altitude = -2.0;
 
-px4_takeoff(client, config);
-pause(10);
+    px4_takeoff(client, config);
+    pause(10);
 
-% CONTROLLER SELECTION
-CONTROL_MODE = 'position';
+    % CONTROLLER SELECTION
+    CONTROL_MODE = 'position';
 
-% switch to control mode
-px4_switch_control_mode(client, CONTROL_MODE, config);
-pause(2)
+    % switch to control mode
+    px4_switch_control_mode(client, CONTROL_MODE, config);
+    pause(2)
 
-px4_send_trajectory(client, 0, 0, -2, 0, config); % hover at (0, 0, -2), yaw = 0
-pause(10);
+    px4_send_trajectory(client, 0, 0, -2, 0, config); % hover at (0, 0, -2), yaw = 0
+    pause(10);
 
-px4_initiate_landing(client, config);
-pause(5);
-px4_disarm_drone(client, config);
+    px4_initiate_landing(client, config);
+    pause(5);
+    px4_disarm_drone(client, config);
+end
